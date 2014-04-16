caf00f15e435ff85e6919e02a78d8189b72fc1ea
James Carman <jcarman@apache.org>
2014-04-16 12:52:10 -0400

    Removing round robin.

--- src/test/scala/com/carmanconsulting/akka/sandbox/HelloTest.scala
+++ src/test/scala/com/carmanconsulting/akka/sandbox/HelloTest.scala
@@ -2,13 +2,12 @@ package com.carmanconsulting.akka.sandbox
 
 import org.junit.Test
 import akka.actor.Props
-import akka.routing.RoundRobinPool
 
 class HelloTest extends AkkaTestCase {
 
   @Test
   def testHello(): Unit = {
-    val hello = system.actorOf(RoundRobinPool(5).props(Props[HelloActor]), "hello")
+    val hello = system.actorOf(Props[HelloActor], "hello")
     hello ! "Akka"
     expectMsg("Hello, Akka!")
   }
